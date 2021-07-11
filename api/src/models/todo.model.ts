51d17efb41786df04b9536aebc48dc2e685273b1
Adam Alvis <adam.alvis@gmail.com>
2021-07-11 17:43:38 -0400

    generated react app and loopback api added mongodb datasource and todo entity to api

---
+++ api/src/models/todo.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  text: string;
+
+  @property({
+    type: 'string',
+  })
+  isComplete?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  sequence: number;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
